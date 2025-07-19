0cebe8adc0d1f4175ff122c3d3293a9b252c9916
cuogne <cuog.nc116@gmail.com>
2025-07-20 00:08:52 +0700

    update lunar day

--- commands/date/date.js
+++ commands/date/date.js
@@ -1,8 +1,45 @@
+import { SolarDate } from "@nghiavuive/lunar_date_vi";
+// check this lib calc lunar date: https://github.com/nacana22/lunar-date
+
 export async function dateCommand(interaction) {
+    await interaction.deferReply();
+
     const nowDate = new Date();
     const dateStr = nowDate.toLocaleString('vi-VN', {
         timeZone: 'Asia/Ho_Chi_Minh'
-    });
+    }).split(" ");
 
-    await interaction.reply(`${dateStr}`);
+    const timeNow = dateStr[0]; // thoi gian
+    const solarDate = dateStr[1]; // duong lich
+
+    const lunar = new SolarDate(nowDate);
+    const lunarDate = lunar.toLunarDate().day + '/'
+        + lunar.toLunarDate().month + '/' + lunar.toLunarDate().year; // am lich
+
+    await interaction.editReply({
+        embeds: [{
+            color: 0x4285f4,
+            title: "📅 Ngày hiện tại",
+            fields: [
+                {
+                    name: '🌞 Dương lịch',
+                    value: solarDate,
+                    inline: false
+                },
+                {
+                    name: '🌙 Âm lịch',
+                    value: lunarDate,
+                    inline: false
+                },
+                {
+                    name: '🕰️ Thời gian',
+                    value: timeNow,
+                    inline: false
+                }
+            ],
+            footer: {
+                text: 'GMT+7 (Việt Nam)'
+            }
+        }]
+    })
 }
