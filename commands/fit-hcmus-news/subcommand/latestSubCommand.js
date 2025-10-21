eb5f8585bc0a786b494589f62167b9990dcf9253
cuogne <cuog.nc116@gmail.com>
2025-10-21 15:51:13 +0700

    fix: get latest data

--- commands/fit-hcmus-news/subcommand/latestSubCommand.js
+++ commands/fit-hcmus-news/subcommand/latestSubCommand.js
@@ -1,19 +1,37 @@
-// import { getLatestNews } from '../rss/getLatestNews.js';
-
-import { sendNews } from "../main/sendNews.js";
+import { feedLinks } from "../resource/link.js"
+import { crawlRssNews } from "../script/crawl-rss.js";
+import { crawlHTMLNews } from "../script/crawl-html.js";
 
 export async function latestSubCommand(interaction) {
-    // try {
-    //     await interaction.deferReply();
+    try {
+        await interaction.deferReply();
 
-    //     const news = await getLatestNews();
-    //     await interaction.editReply({
-    //         content: `📰 | **${news.title}**\n\n${news.link}`,
-    //     });
-    // } catch (error) {
-    //     await interaction.editReply({
-    //         content: '❌ | Failed to fetch the latest news.',
-    //     });
-    //     console.error(error);
-    // }
+        const categoryOption = interaction.options.getString('category');
+
+        const feed = feedLinks.find(f => f.category === categoryOption);
+        const type = feed.type
+        const url = feed ? feed.url : null;
+
+        console.log('Selected category:', categoryOption);
+        console.log('Feed URL:', url);
+        console.log('Feed Type:', type);
+
+        if  (type == 'rss') {
+            const newsData = await crawlRssNews(url, categoryOption);
+            await interaction.editReply(
+                `📰 | **${newsData.title}**\n\n${newsData.url}`
+            );
+        } else if (type == 'html') {
+            const htmlNewsData = await crawlHTMLNews(url, categoryOption);
+            await interaction.editReply(
+                `📰 | **${htmlNewsData.title}**\n\n${htmlNewsData.url}`
+            );
+        }
+
+    } catch (error) {
+        await interaction.editReply({
+            content: '❌ | Failed to fetch the latest news.',
+        });
+        console.error(error);
+    }
 }
